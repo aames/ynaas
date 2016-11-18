cc5b763539541b3e3d26ce2f29b29152cf45ec23
Andrew <andrew.amesbury@loyalty.co.nz>
2016-11-18 21:51:19 +1300

    minor typo and version error

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
     name='ye-na-aas',
-    version='0.1.0',
+    version='1.0.0',
     packages=['yeah_nah_aas'],
     license='MIT',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         'flask'
     ]
