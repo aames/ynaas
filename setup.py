315aaf3ecad45205a1f12388164fa187f1bba20e
Andrew <andrew.amesbury@loyalty.co.nz>
2016-11-18 21:52:41 +1300

    remove setup package

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from distutils.core import setup
 setup(
     name='ye-na-aas',
     version='1.0.0',
-    packages=['yeah_nah_aas'],
     license='MIT',
     long_description=open('README.md').read(),
     install_requires=[
